0523e1de17595b885d136ef63dcbfd4a6e674beb
fatemehvalipour <fatemeh.vpasha@gmail.com>
2021-12-03 20:05:38 +0330

    Adds comment

--- logic.js
+++ logic.js
@@ -1,36 +1,46 @@
+// function that is called when user clicks on submit button - if entered name is valid, the name would be sent to url
 function submit(){
+    // get name from textbox
     var name = document.getElementById("name").value
     var validName = true
+    // check if the box is empty or not 
     if(name == null){
         alert("Please Enter a name!")
         validName = false
     }
+    // check if the text is loger than 255 characters or not 
     if(name.length > 255){
         alert("You are only allowed to enter a name with maximum 255 characters length")
         validName = false
     }
+    //check if text contains english characters and numbers or not
     var regex = /^[A-Za-z\s]+$/
     if(!regex.test(name)){
         alert("You are only allowed to enter space and english characters")
         validName = false
     }
+    // if name is valid, the request will be sent to the given server
     if(validName == true){
         prediction(name)
     }
 }
 
+// sends request and shows response in prediction section 
 function prediction(name){
+    // fetch the request 
     fetch('https://api.genderize.io/?name=' + name)
     .then(Response => Response.json())
     .catch(error => console.error("network error"))
     .then(data =>
         {
+            // set variables from the response
             var gender = data["gender"]
             var probability = data["probability"]
             if(gender == null){
                 gender = "Sorry! I don't know"
                 probability = "-"
             }
+            // put the variables in right place of the box
             document.getElementById("gender").innerHTML = gender
             document.getElementById("probability").innerHTML = probability
             // if we have the name in localStorage
@@ -48,7 +58,9 @@ function prediction(name){
         })
 }
 
+// called when user clicks on clear button 
 function clearRecord(){
+    // removes name in localStorage and changes the text to "not saved" and the box becomes red
     var name = document.getElementById("name").value
     localStorage.removeItem(name)
     document.getElementById("SavedAnswer").innerHTML = "not Saved"
@@ -56,7 +68,9 @@ function clearRecord(){
     document.getElementById("saved").style.borderColor = 'red'
 }
 
+// called when user clicks on save button
 function save(){
+    // retrievs user choice between radioButton options
     const choices = document.querySelectorAll('input[name="maleFemale"]')
     let choice
     for (const option of choices) {
@@ -64,10 +78,12 @@ function save(){
             choice = option.value
         } 
     }
+    // retrieve name from text box and checks if radioButton is null or not 
     var name = document.getElementById("name").value
     if(choice == null){
         alert("Please choose between male or female!")
     }
+    // saves name into localStorage and changes the design of the box
     localStorage.setItem(name, choice)
     document.getElementById("SavedAnswer").innerHTML = localStorage.getItem(name)
     document.getElementById("clear").style.visibility = 'visible'
